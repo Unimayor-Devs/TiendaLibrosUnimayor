9bc607cb7d727744564bfd8b8d89141dcf4d09f7
LCCollazos <luiscollazosc98@gmail.com>
2024-05-27 23:01:45 -0500

    Desarrollo carrito de compras e inventario

--- frontend/src/pages/GestionCompras/User/UserShoppingCartScreen.js
+++ frontend/src/pages/GestionCompras/User/UserShoppingCartScreen.js
@@ -1,30 +1,83 @@
-import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import React, { useState, useEffect, useContext } from 'react';
+import { getAllBooks, deleteBook } from '../../../services/bookService';
+import Navbar from '../../../components/Navbar';
+import './UserShoppingCartScreen.css';
+import './common-styles.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookJournalWhills, faCartShopping, faEdit, faPlus, faShop, faShoppingBag, faShoppingBasket, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FaBuyNLarge } from 'react-icons/fa';
 
 const UserShoppingCartScreen = () => {
-  const [cartItems, setCartItems] = useState([
-    { id: 1, title: 'Libro 1', price: 10.99, quantity: 1 },
-    { id: 2, title: 'Libro 2', price: 12.99, quantity: 2 },
-  ]);
+  const location = useLocation();
+  const { booksShopping: initialBooksShopping } = location.state || { booksShopping: [] };
+  const [booksShopping, setBooksShopping] = useState(initialBooksShopping);
 
   const handleRemoveItem = (itemId) => {
-    const updatedCart = cartItems.filter(item => item.id !== itemId);
-    setCartItems(updatedCart);
+    const updatedCart = booksShopping.filter(item => item.id !== itemId);
+    setBooksShopping(updatedCart);
   };
+
+  const formatCurrency = (value) => {
+    return `$ ${value.toLocaleString()}`;
+  };
+
+  const handleCheckout = () => {
+    // Simular la lógica de compra y limpiar el carrito
+    alert('¡Compra realizada con éxito!');
+    setBooksShopping([]);
+  };
+
+  const totalAmount = booksShopping.reduce((total, book) => total + book.titulo, 0);
 
   return (
     <div>
-      <h2>Carrito de Compras</h2>
-      <ul>
-        {cartItems.map(item => (
-          <li key={item.id}>
-            <strong>{item.title}</strong> - ${item.price} - Cantidad: {item.quantity}
-            <button onClick={() => handleRemoveItem(item.id)}>Eliminar</button>
-          </li>
-        ))}
-      </ul>
-      <p>Total: ${cartItems.reduce((total, item) => total + item.price * item.quantity, 0)}</p>
+      <Navbar />
+      <h1>Carrito de Compras</h1>
+      <div className="user-books-container">
+        <div className="table-container">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Libro</th>
+                <th>Valor</th>
+                <th>Eliminar</th>
+              </tr>
+            </thead>
+            <tbody>
+            {booksShopping.length === 0 ? (
+              <tr>
+                <td colSpan="3" className="no-books-message">Aún no hay libros en el carrito.</td>
+              </tr>
+              ) : (
+                booksShopping.map(book => (
+                  <tr key={book.id}>
+                    <td>{book.precio}</td>
+                    <td>{book.titulo}</td>
+                    <td>
+                      <button className='btn btn-danger' onClick={() => handleRemoveItem(book.id)}>
+                        Eliminar  <FontAwesomeIcon icon={faTrash} />
+                      </button>
+                    </td>
+                  </tr>
+                ))
+              )}
+            </tbody>
+            <tfoot>
+              <tr>
+                <td>Total</td>
+                <td colspan="2">
+                  <p>{formatCurrency(totalAmount)}</p>
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+          <button onClick={handleCheckout}>Realizar Compra</button>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default UserShoppingCartScreen;
+
